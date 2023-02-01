802edc545bd605e91e8a12f3469ceafa2525a7c7
IgnacioGomez00 <43622417+IgnacioGomez00@users.noreply.github.com>
2023-02-01 15:10:58 +0100

    Add files via upload

---
+++ TFG_OtherObjects_Image2.py
@@ -0,0 +1,47 @@
+import cv2
+
+#Cargar DNN model y pesos
+prototxt = r"C:\Users\ignac\OneDrive\Escritorio\Python\TFG\dnn_object_detection\model\MobileNetSSD_deploy.prototxt.txt"
+model = r"C:\Users\ignac\OneDrive\Escritorio\Python\TFG\dnn_object_detection\model\MobileNetSSD_deploy.caffemodel"
+
+#Etiquetas de las clases
+classes = {0:"background", 1:"aeroplane", 2:"bicycle",
+          3:"bird", 4:"boat",
+          5:"bottle", 6:"bus",
+          7:"car", 8:"cat",
+          9:"chair", 10:"cow",
+          11:"diningtable", 12:"dog",
+          13:"horse", 14:"motorbike",
+          15:"person", 16:"plant",
+          17:"sheep", 18:"sofa",
+          19:"train", 20:"tv"}
+
+#Cargar el modelo
+net = cv2.dnn.readNetFromCaffe(prototxt, model)
+
+#Lectura de imagen 
+image = cv2.imread(r"C:\Users\ignac\OneDrive\Escritorio\Python\TFG\images\OtherObjects_Image2.png")
+height, width, _ = image.shape
+image_resized = cv2.resize(image, (300, 300))
+
+#Crear una blob
+blob = cv2.dnn.blobFromImage(image_resized, 0.007843, (300, 300), (127.5, 127.5, 127.5))
+
+#Detección de clases y precisión
+net.setInput(blob)
+detections = net.forward()
+for detection in detections[0][0]:
+     print(detection)
+     if detection[2] > 0.45:
+          label = classes[detection[1]]
+          print("Label:", label)
+          box = detection[3:7] * [width, height, width, height]
+          x_start, y_start, x_end, y_end = int(box[0]), int(box[1]), int(box[2]), int(box[3])
+          cv2.rectangle(image, (x_start, y_start), (x_end, y_end), (0, 255, 0), 2)
+          cv2.putText(image, "Conf: {:.2f}".format(detection[2] * 100), (x_start, y_start - 5), 1, 1.2, (255, 0, 0), 2)
+          cv2.putText(image, label, (x_start, y_start - 25), 1, 1.2, (0, 255, 255), 2)
+
+#Imagen por pantalla y finalización de proceso
+cv2.imshow("Image with animals", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ TFG_SignDetection_Video3.py
@@ -0,0 +1,91 @@
+#Importar librerias necesarias
+import cv2
+import numpy as np
+
+#Video de muestreo .avi
+#video_entrada = (r"C:\Users\ignac\OneDrive\Escritorio\Python\TFG\videos\walking.avi")
+
+
+#Lectura de imagen y selección de archivos Haar 
+cap = cv2.VideoCapture(0)
+#fgbg = cv2.createBackgroundSubtractorMOG2()
+
+stop_sign = cv2.CascadeClassifier(r"C:\Users\ignac\OneDrive\Escritorio\Python\TFG\XMLs\cascade_stop_sign.xml")
+noturnleft_sign = cv2.CascadeClassifier(r"C:\Users\ignac\OneDrive\Escritorio\Python\TFG\XMLs\no_turn_left.xml")
+noturnright_sign = cv2.CascadeClassifier(r"C:\Users\ignac\OneDrive\Escritorio\Python\TFG\XMLs\no_turn_right.xml")
+#stop_sign = cv2.CascadeClassifier(r"C:\Users\ignac\OneDrive\Escritorio\Python\TFG\haar_cascade_train\stop-\classifier\cascade_stop.xml")
+#noturnleft_sign = cv2.CascadeClassifier(r"C:\Users\ignac\OneDrive\Escritorio\Python\TFG\haar_cascade_train\giro izquierda-\classifier\cascade.xml")
+#noturnright_sign = cv2.CascadeClassifier(r"C:\Users\ignac\OneDrive\Escritorio\Python\TFG\haar_cascade_train\giro derecha-\classifier\cascade.xml")
+caution_sign = cv2.CascadeClassifier(r"C:\Users\ignac\OneDrive\Escritorio\Python\TFG\XMLs\caution.xml")
+right_sign = cv2.CascadeClassifier(r"C:\Users\ignac\OneDrive\Escritorio\Python\TFG\XMLs\right.xml")
+left_sign = cv2.CascadeClassifier(r"C:\Users\ignac\OneDrive\Escritorio\Python\TFG\XMLs\left.xml")
+yield_sign = cv2.CascadeClassifier(r"C:\Users\ignac\OneDrive\Escritorio\Python\TFG\XMLs\yield.xml")
+parking_sign = cv2.CascadeClassifier(r"C:\Users\ignac\OneDrive\Escritorio\Python\TFG\XMLs\parking.xml")
+
+while cap.isOpened(): 
+
+
+	#Definir variables y leer el primer frame
+	ret, frame = cap.read()
+	#fgbg.apply(frame)
+
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	color_green = (0,255,0) 
+	color_yellow = (0,255,255)
+
+	#Modelo de Haar Cascade
+	stop_sign_scaled = stop_sign.detectMultiScale(gray, 1.2, minNeighbors=10 )
+	noturnleft_sign_scaled = noturnleft_sign.detectMultiScale(gray, 1.1, minNeighbors=4 )
+	noturnright_sign_scaled = noturnright_sign.detectMultiScale(gray, 1.1, minNeighbors=3 )
+	caution_sign_scaled = caution_sign.detectMultiScale(gray, 1.1, minNeighbors=3 )
+	right_sign_scaled = right_sign.detectMultiScale(gray, 1.1, minNeighbors=3 )
+	left_sign_scaled = left_sign.detectMultiScale(gray, 1.1, 3)
+	yield_sign_scaled = yield_sign.detectMultiScale(gray, 1.1, 3)
+	parking_sign_scaled = parking_sign.detectMultiScale(gray, 1.1, 3)
+
+	#Dibuja rectangulo en las coordenadas del objeto y texto encima del rectangulo identificando su clase
+
+	for (x,y,w,h) in stop_sign_scaled:
+		cv2.rectangle(frame, (x,y), (x+w, y+h), color_green, 2)
+		cv2.putText(frame, "stop", (x,y-5), 1, 1.2, color_yellow, 2)
+
+	for (x,y,w,h) in noturnleft_sign_scaled:
+		cv2.rectangle(frame, (x,y), (x+w, y+h), color_green, 2)
+		cv2.putText(frame, "no turn left", (x,y-5), 1, 1.2, color_yellow, 2)
+	
+	for (x,y,w,h) in noturnright_sign_scaled:
+		cv2.rectangle(frame, (x,y), (x+w, y+h), color_green, 2)
+		cv2.putText(frame, "no turn right", (x,y-5), 1, 1.2, color_yellow, 2)
+
+	for (x,y,w,h) in caution_sign_scaled:
+		cv2.rectangle(frame, (x,y), (x+w, y+h), color_green, 2)
+		cv2.putText(frame, "caution", (x,y-5), 1, 1.2, color_yellow, 2)
+
+	for (x,y,w,h) in right_sign_scaled:
+		cv2.rectangle(frame, (x,y), (x+w, y+h), color_green, 2)
+		cv2.putText(frame, "right", (x,y-5), 1, 1.2, color_yellow, 2)
+
+	for (x,y,w,h) in left_sign_scaled:
+		cv2.rectangle(frame, (x,y), (x+w, y+h), color_green, 2)
+		cv2.putText(frame, "left", (x,y-5), 1, 1.2, color_yellow, 2)		
+
+	for (x,y,w,h) in yield_sign_scaled:
+		cv2.rectangle(frame, (x,y), (x+w, y+h), color_green, 2)
+		cv2.putText(frame, "yield", (x,y-5), 1, 1.2, color_yellow, 2)	
+
+	for (x,y,w,h) in parking_sign_scaled:
+		cv2.rectangle(frame, (x,y), (x+w, y+h), color_green, 2)
+		cv2.putText(frame, "parking", (x,y-5), 1, 1.2, color_yellow, 2)	
+
+	#Imagen por pantalla
+	#cv2.imshow('BackgroundSubtractor',fgbg)
+	#cv2.imshow('Gray',gray)
+	cv2.imshow("Result", frame)
+
+	#Finalización del proceso
+	if cv2.waitKey(30) == 27:
+		break
+
+cap.release()
+cv2.destroyAllWindows()
+	
